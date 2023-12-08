8293a48495dbc0c62cba574fd13b7b787615734d
Christian TOGBEDJI <151915885+christiantogbedji@users.noreply.github.com>
2023-12-08 16:54:51 +0100

    Update main.py

--- notebooks/IndicateursSecheresse/data_processing/main.py
+++ notebooks/IndicateursSecheresse/data_processing/main.py
@@ -4,24 +4,24 @@ from water_tracker import WaterTracker
 def main():
     # Replace "" with your actual API key
     api_key = ""
-    indicators = ["deep groundwater", "rainfall", "groundwater"]
+    indicateurs = ["nappes", "pluviométrie", "nappes profondes"]
 
     wt = WaterTracker(api_key)
 
     # If you haven't built station data and downloaded all timeseries yet uncomment the following lines to do so
-    # wt.build_stations_data()
-    # wt.download_all_timeseries()
+    #wt.build_stations_data()
+    #wt.download_all_timeseries()
 
     # For each indicator, load existing data and generate the plot
-    for indicator in indicators:
+    for indicateur in indicateurs:
         # Comment the following line if you don't want to recalculate standardized indicators on each run
-        wt.process(indicator=indicator)
+        wt.process(indicateur=indicateur)
 
         # Load existing data
         wt.load()
 
         # Generate the plot for drought distribution in France
-        df_levels = wt.plot_counts_france(indicator=indicator)
+        df_levels = wt.plot_counts_france(indicateur=indicateur)
 
         # Show the plot
         plt.show()
