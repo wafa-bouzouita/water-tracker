84932617e0cf8af9ac0af715611c08e4a1fdc419
claire2023 <ubuntu@JMLPT1218>
2024-01-16 15:03:29 +0100

    Mise à jour de la visualisation des données

--- notebooks/ImpactSecheresse_Agri_Et_Bat/Visualisation_sm.py
+++ notebooks/ImpactSecheresse_Agri_Et_Bat/Visualisation_sm.py
@@ -7,41 +7,49 @@ import xarray as xr
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib import cm
+import altair as alt
 
-
-def humidity_class(val):    
-    if   0<= val <15.0:
-        return "Extremement sec"
-    elif 15.0<= val <30.0:
-        return "Très sec"
-    elif 30.0 <= val<50.0:
-        return "Modèrement sec"
-    elif 50.0 <= val<65.0:
-        return "Humidité normale"
-    elif 65.0 <= val<75.0:
-        return "Modèrement humide"
-    elif 75.0 <= val<90.0:
-        return "Très humide"
-    elif 90<= val<100.0:
-        return "Extremement humide"
-    else:
-        return "Nan"
     
 #Chargement des données 
-data=pd.read_csv("data/archive2.csv", sep=",")
+data=pd.read_csv("data/data_agri_bat3.csv", sep=",")
+#data['time']=pd.to_datetime(data['time'])
 print (data.dtypes)
 
 #transformation de la colonne time en datetime
-data['time']=pd.to_datetime(data['time'], format="")
+#data['time']=pd.to_datetime(data['time'])
+class_color=['#D0D3D4','#21618C', '#3498DB', '#85C1E9', '#32CD42', '#FFE333', '#FFA533', '#FF3342']
+classes=['Nan','Extremement humide','Très humide','Modèrement humide','Humidité normale','Modèrement sec','Très sec','Extremement sec']
 
-#Classification des valeurs d'indice d'humidite 
-data['class_sm']=data['sm'].astype(float).apply(humidity_class)
 
+#class_color={'A':'#D0D3D4','B':'#21618C', 'C':'#3498DB', 'D':'#85C1E9', 'E':'#32CD42', 'F':'#FFE333', 'G':'#FFA533', 'H':'#FF3342'}
 #Regroupement des données en fonction de la classe et le mois
-data_cross=pd.crosstab( data['time'], data['class_sm'], normalize="index").sort_index(axis=0,ascending=False)
+#data_cross=pd.crosstab( data['time'], data['class_sm'], normalize="index").sort_index(axis=0,ascending=True)
 
+vis_sm=(alt
+    .Chart(data, title="Humidité des sols nécessaire à l'agriculture")
+    .transform_calculate(classes=f"-indexof({classes}, datum.class_sm)")
+    .mark_bar()
+    .encode(
+       x=alt.X('time:N').title(None),
+       y=alt.Y('count(class_sm)').stack("normalize").title(" % du territoire"), 
+       color=alt.Color('class_sm:N', sort=classes).scale( range=class_color).title(None),
+       order="classes:N"
+    )
+)
 #Affichage des données 
-st.title("Indice d'humidité des sols")
-print (data_cross[:10])
-st.bar_chart(data_cross[:10], width=5)
+#st.title("Impact de la sécheresse sur l'agriculture et les batiments")
+st.altair_chart(vis_sm, use_container_width=True)
 
+vis_usm=(alt
+    .Chart(data, title="Indice d'humidité uniforme des sols")
+    .transform_calculate(classes=f"-indexof({classes}, datum.class_usm)")
+    .mark_bar()
+    .encode(
+       x=alt.X('time:N').title(None),
+       y=alt.Y('count(class_usm)').stack("normalize").title(" % du territoire"), 
+       color=alt.Color('class_usm:N', sort=classes).scale( range=class_color).title(None),
+       order="classes:N"
+    )
+)
+
+st.altair_chart(vis_usm, use_container_width=True)
